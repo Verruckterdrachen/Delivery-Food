b219c54d72d7cb053c0a24243010e15005844609
verruckterdrachen <verruckterdrachen@gmail.com>
2021-08-01 12:51:18 +0300

    feat: add header

---
+++ #src/js/files/script.js
@@ -0,0 +1,23 @@
+const headerElement = document.querySelector('.header');
+
+const callback = function (entries, observer) {
+  if (entries[0].isIntersecting) {
+    headerElement.classList.remove('_scroll');
+  } else {
+    headerElement.classList.add('_scroll');
+  }
+};
+
+const headerObserver = new IntersectionObserver(callback);
+headerObserver.observe(headerElement);
+
+
+window.onload = function () {
+  document.addEventListener("click", documentActions);
+  function documentActions(e) {
+    const targetElement = e.target;
+    if (targetElement.classList.contains('search-form__icon')) {
+      document.querySelector('.search-form').classList.toggle('_active');
+    }
+  }
+}
